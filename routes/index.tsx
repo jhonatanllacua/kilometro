1f94522b96a2d909b548ccbd8dd557546298ff31
jhonatanllacua <75458455+jhonatanllacua@users.noreply.github.com>
2022-12-02 16:09:32 -0500

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,21 @@ export default function Home() {
   return (
     <>
     <title>Kilometro 69</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Kilometro 69" />
+    <meta property="og:description" content="Ofrecemos los mejores platillos de gran selección, como también bebidas unicas" />
+    <meta property="og:url" content="..." />
+    <meta property="og:site_name" content="Facemash - Who is so beauty" />
+    <meta property="og:image" content="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Frestaurant%2F&psig=AOvVaw3B6XGPV34ruUNXWagHwXUV&ust=1670101672004000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNCgh9_r2_sCFQAAAAAdAAAAABAF" />
+    <meta property="og:image:width" content="640">
+    <meta property="og:image:height" content="300">
+    <meta name="description" content="A la hora de escribir una meta descripción, manténla entre 140 y 160 caracteres para que Google pueda mostrar tu mensaje completo. ¡No olvides incluir tu palabra clave!">
+    <meta name="keywords" content="kilometro69 restobar comida" />
+    <meta name="Autor" content="citeO" />
+    <meta name="robots" content="index,follow" />
       <link rel="stylesheet" href="/style.css" />
         <br/>
         <br/>
