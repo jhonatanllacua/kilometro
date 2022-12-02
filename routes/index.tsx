8958e44f51123355da8a8cc01929c49da5cf076b
jhonatanllacua <75458455+jhonatanllacua@users.noreply.github.com>
2022-12-02 15:49:28 -0500

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 class="Titulo" >BIENVENIDO A KILOMETRO 69</h1>
         <h1 class="subtitle">Puedes ver nuestra carta, en la siguiente pagina.</h1>
         <br/>
-        <a href="/carta"><button class="button-carta">Ver Carta</button></a>
+        <a href="/carta"><button class="button-carta">&nbsp;&nbsp; Ver Carta ➙ &nbsp;&nbsp;</button></a>
       </div>
     </>
   );
