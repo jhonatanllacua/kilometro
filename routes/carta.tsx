9afba60bee77e2b533f5c69ca23abdd1b43300a1
jhonatan6llacua@gmail.com <jhonatan6llacua@gmail.com>
2022-12-02 10:54:58 -0500

    inserta Mensaje aquí

---
+++ routes/carta.tsx
@@ -0,0 +1,53 @@
+
+export default function Home() {
+  return (
+    <>
+            <title>Kilometro 69</title>
+        <link rel="stylesheet" href="/style.css" />
+    <body>
+        
+    <center>
+       
+    <div class="container" id="container">
+      
+      <div class="book-content">
+          <div class="book">
+              <div class="face-front" id="portada"></div>
+              <div class="face-back" id="trsf">
+                  <img class="imach1" src="/img/carta_uno.png" width="425"/>
+              </div>
+          </div>
+
+          <div class="book">
+              <div class="face-front">
+                  <img class="imach" src="/img/carta_dos.png" width="425" />
+              </div>
+              <div class="face-back">
+                  <img class="imach1" src="/img/carta_tres.png" width="425"/>
+               </div>
+          </div>
+
+          <div class="book">
+              <div class="face-front">
+                  <img class="imach" src="/img/carta_cuatro.png" width="425"/>
+              </div>
+              <div class="face-back">
+                  <img class="imach1" src="/img/carta_cinco.png" width="425"/>
+               </div>
+          </div>
+
+          <div class="book">
+              <div class="face-front">
+                  <img class="imach" src="/img/carta_seis.png" width="425"/>
+              </div>
+              <div class="face-back" id="portada-back">
+              </div>
+          </div>
+      </div>
+  </div>
+  </center>
+
+  </body>
+    </>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <>
+    <title>Kilometro 69</title>
+      <link rel="stylesheet" href="/style.css" />
+      <div class="p-4 mx-auto max-w-screen-md">
+        <h1 class="Titulo" >BIENVENIDO A KILOMETRO 69</h1>
+        <h1 class="subtitle">Puedes ver nuestra carta, en la siguiente pagina.</h1>
+        <br/>
+        <br/>
+        <a href="/carta"><button class="button-carta">Ver Carta</button></a>
+      </div>
+    </>
+  );
+}
