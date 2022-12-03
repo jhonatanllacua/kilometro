5f349eff6eac6859f2feb806baaa236c692a5b8a
jhonatanllacua <75458455+jhonatanllacua@users.noreply.github.com>
2022-12-02 19:18:43 -0500

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,11 @@
+import { Head } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
     <>
-    <head>
+    <Head>
     <title>Kilometro 69</title>
+
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -12,18 +14,16 @@ export default function Home() {
     <meta property="og:description" content="Ofrecemos los mejores platillos de gran selección, como también bebidas unicas" />
     <meta property="og:url" content="..." />
     <meta property="og:site_name" content="Facemash - Who is so beauty" />
-    <meta property="og:image" content="https://elcomercio.pe/resizer/e4DTSpRdYEc4RI1ZjPoj8rgNWpk=/1200x900/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/ABAP7ION7FFTZNOB7GT2YY27LE.jpg" />
+    <meta property="og:image" content="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Frestaurant%2F&psig=AOvVaw3B6XGPV34ruUNXWagHwXUV&ust=1670101672004000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNCgh9_r2_sCFQAAAAAdAAAAABAF" />
     <meta property="og:image:width" content="640"/>
     <meta property="og:image:height" content="300"/>
     <meta name="description" content="A la hora de escribir una meta descripción, manténla entre 140 y 160 caracteres para que Google pueda mostrar tu mensaje completo. ¡No olvides incluir tu palabra clave!"/>
     <meta name="keywords" content="kilometro69 restobar comida" />
     <meta name="Autor" content="citeO" />
-    <meta name="robots" content="index,follow" />
-      
+  <meta name="robots" content="index,follow" />
+
     <link rel="stylesheet" href="/style.css" />
-      
-      </head>
-<body>
+      </Head>
         <br/>
         <br/>
         <br/>
@@ -37,7 +37,6 @@ export default function Home() {
         <br/>
         <a href="/carta"><button class="button-carta">&nbsp;&nbsp; Ver Carta ➙ &nbsp;&nbsp;</button></a>
       </div>
-</body>
     </>
   );
 }
